cec8754fe6d204ba94fc43ce46c649e969e82cd8
JiangYinZhu <1078141408@qq.com>
2025-07-18 21:51:46 +0800

    feat:HTTP请求会话协议处理

--- api-gateway-core/src/test/java/cn/jing/gateway/test/ApiTest.java
+++ api-gateway-core/src/test/java/cn/jing/gateway/test/ApiTest.java
@@ -1,65 +1,34 @@
 package cn.jing.gateway.test;
 
-import org.apache.dubbo.config.ApplicationConfig;
-import org.apache.dubbo.config.ReferenceConfig;
-import org.apache.dubbo.config.RegistryConfig;
-import org.apache.dubbo.config.bootstrap.DubboBootstrap;
-import org.apache.dubbo.config.utils.ReferenceConfigCache;
-import org.apache.dubbo.rpc.service.GenericService;
+import cn.jing.gateway.session.SessionServer;
+import io.netty.channel.Channel;
 import org.junit.Test;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ApiTest {
 
+    private final Logger logger = LoggerFactory.getLogger(ApiTest.class);
+
     @Test
-    public void test_rpc() {
+    public void test() throws ExecutionException, InterruptedException {
+        SessionServer server = new SessionServer();
 
-        ApplicationConfig application = new ApplicationConfig();
-        application.setName("api-gateway-test");
-        application.setQosEnable(false);
+        Future<Channel> future = Executors.newFixedThreadPool(2).submit(server);
+        Channel channel = future.get();
 
-        RegistryConfig registry = new RegistryConfig();
-        registry.setAddress("zookeeper://127.0.0.1:2181");
-        registry.setRegister(false);
+        if (null == channel) throw new RuntimeException("netty server start error channel is null");
 
-        ReferenceConfig<GenericService> reference = new ReferenceConfig<>();
-        reference.setInterface("cn.bugstack.gateway.rpc.IActivityBooth");
-        reference.setVersion("1.0.0");
-        reference.setGeneric("true");
+        while (!channel.isActive()) {
+            logger.info("NettyServer启动服务 ...");
+            Thread.sleep(500);
+        }
+        logger.info("NettyServer启动服务完成 {}", channel.localAddress());
 
-        DubboBootstrap bootstrap = DubboBootstrap.getInstance();
-        bootstrap.application(application)
-                .registry(registry)
-                .reference(reference)
-                .start();
-
-        ReferenceConfigCache cache = ReferenceConfigCache.getCache();
-        GenericService genericService = cache.get(reference);
-
-//        Object result = genericService.$invoke("sayHi", new String[]{"java.lang.String"}, new Object[]{"world"});
-//        Map<String, Object> allRequestParams = new HashMap();
-//        allRequestParams.put("name", "小傅哥");
-//        allRequestParams.put("uid", "10001");
-//        Object result = genericService.$invoke("insert", new String[]{"java.lang.Object"}, new Object[]{allRequestParams});
-
-        String[] parameterTypes = new String[]{"java.lang.String", "cn.bugstack.gateway.rpc.dto.XReq"};
-
-        Map<String, Object> params01 = new HashMap<>();
-//        params.put("class", "cn.bugstack.gateway.rpc.dto.XReq");
-        params01.put("str", "10001");
-
-        Map<String, Object> params02 = new HashMap<>();
-//        params.put("str", "10001");
-        params02.put("uid", "10001");
-        params02.put("name", "小傅哥");
-
-//        Object user = genericService.$invoke("sayHi", new String[]{"java.lang.String"}, params.values().toArray());
-//        Object user = genericService.$invoke("insert", new String[]{"cn.bugstack.gateway.rpc.dto.XReq"}, new Object[]{params});
-        Object user = genericService.$invoke("test", new String[]{"java.lang.String", "cn.bugstack.gateway.rpc.dto.XReq"}, new Object[]{params01.values().toArray()[0], params02});
-
-        System.out.println(user);
+        Thread.sleep(Long.MAX_VALUE);
     }
 
 }
